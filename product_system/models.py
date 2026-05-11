e5fdbb024555bac2a53760e478d273b8f60c2859
KevinArchive1 <haroldvelena27@gmail.com>
2026-05-11 11:09:35 +0800

    FINALS HANDS-ON QUIZ #3: Simple Online Product Display System

---
+++ product_system/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.FloatField()
+
+    def product_info(self):
+        return f"{self.name} - ${self.price}"
+    
+    def __str__(self):
+        return self.name
